5587574aa687d3e4aa63cde6b333bde90eaf40c0
Matheus <mathgomes007@hotmail.com>
2025-05-05 15:09:37 -0300

    guards suitability in hospital unity

--- src/components/hospitalUnity/hospitalUnity.controller.ts
+++ src/components/hospitalUnity/hospitalUnity.controller.ts
@@ -2,31 +2,31 @@ import { Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards } fro
 import { UUID } from "crypto";
 import { HospitalUnityService } from "./hospitalUnity.service";
 import { JwtAuthGuard } from "../auth/login/jwt.guard";
-import { IsAdminGuard } from "../auth/isAdmin/isAdmin.guard";
+import { IsProfessionalGuard } from "../auth/isProfessional/isProfessional.guard";
 
 @Controller('hospital-unity')
 export class HospitalUnityController {
     constructor(private readonly service: HospitalUnityService) {}
 
-    @UseGuards(JwtAuthGuard, IsAdminGuard)
+    @UseGuards(JwtAuthGuard, IsProfessionalGuard)
     @Get()
     async findAllHospitalUnities(@Query() query: any): Promise<any> {
         return await this.service.findAllHospitalUnities(query);
     }
 
-    @UseGuards(JwtAuthGuard, IsAdminGuard)
+    @UseGuards(JwtAuthGuard, IsProfessionalGuard)
     @Post()
     async createHospitalUnity(@Body() data: any): Promise<any> {
         return await this.service.createHospitalUnity(data);
     }
 
-    @UseGuards(JwtAuthGuard, IsAdminGuard)
+    @UseGuards(JwtAuthGuard, IsProfessionalGuard)
     @Put(":id")
     async updateHospitalUnity(@Param('id') id: UUID, @Body() data: any): Promise<any> {
         return await this.service.updateHospitalUnity(id, data);
     }
 
-    @UseGuards(JwtAuthGuard, IsAdminGuard)
+    @UseGuards(JwtAuthGuard, IsProfessionalGuard)
     @Delete(":id")
     async deleteHospitalUnity(@Param('id') id: UUID): Promise<any> {
         return await this.service.deleteHospitalUnity(id);
